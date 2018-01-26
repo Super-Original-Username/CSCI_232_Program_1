3edc1f272336a8cd4bb3e82b4d9389909dd56573
aswordlessninja <aswordlessninja@gmail.com>
2018-01-26 16:07:23 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void foo(){
+        System.out.println("Oh boi here I come!");
+    }
+    public static void main(String[] args){
+        foo();
+    }
+
+}
